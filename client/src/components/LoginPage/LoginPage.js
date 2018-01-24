66ab6fe3e5f1d71b3ea7f96265469fdf3fed548e
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-01-23 20:49:59 -0800

    Added styling for LoginPage component

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './LoginPage.css';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button>
-      <a href="/auth/google">Login with Google</a>
-    </button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expenses Full Stack App</h1>
+      <p>It's time to get your expenses under control</p>
+      <button className="button">
+        <a href="/auth/google" className="nostyle">Login with Google</a>
+      </button>
+    </div>
   </div>
 );
 
