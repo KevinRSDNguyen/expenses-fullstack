bcaf30edecff928bf9baf2210f2e63001d706eef
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-01-24 15:57:05 -0800

    Added css for ExpenseList and ExpenseListItem components

--- client/src/components/ExpenseListItem/ExpenseListItem.js
+++ client/src/components/ExpenseListItem/ExpenseListItem.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
+import './ExpenseListItem.css';
 
-const ExpenseListItem = ({description, amount, createdAt, _id}) => {
+const ExpenseListItem = ({ description, amount, createdAt, _id }) => {
   return (
-    <div>
-      <Link to={`/edit/${_id}`}>
-        <h3>{description}</h3>
-      </Link>
-      <p>
-        {numeral(amount).format('$0,0.00')} 
-        - 
-        {moment(createdAt).format("MMMM Do YYYY")}
-      </p>
-    </div>
+    <Link className="list-item" to={`/edit/${_id}`}>
+      <div>
+        <h3 className="list-item__title">{description}</h3>
+        <span className="list-item__sub-title">{moment(createdAt).format("MMMM Do YYYY")}</span>
+      </div>
+      <h3 className="list-item__data">{numeral(amount).format('$0,0.00')}</h3>
+    </Link>
   );
 };
 
