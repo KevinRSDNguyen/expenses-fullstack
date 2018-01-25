666d029b53117feda670e96d8c48e13c0bf9651a
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-01-24 16:36:23 -0800

    setup express.static for production

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ app.use(passport.session());
 require('./routes/expensesRoutes')(app);
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
